9cffb61d8a3dd9df25925905ae53dd6f32838a3f
Arthur Niedzwiecki <aniedz@cs.uni-bremen.de>
2020-08-25 10:49:53 +0200

    [assembly] Add general mock for preparing phases

--- cram_boxy/cram_boxy_assembly_demo/src/phase-mock.lisp
+++ cram_boxy/cram_boxy_assembly_demo/src/phase-mock.lisp
@@ -1,26 +1,83 @@
 (in-package :demo)
 
-(defparameter *phases*
-  `((:chassis :holder-plane-horizontal :horizontal-attachment)
-    (:bottom-wing :chassis :wing-attachment)
-    (:underbody :bottom-wing :body-attachment)
-    ;; underbody -> rear-wing
-    (:upper-body :underbody :body-on-body)
-    (:bolt-1 :upper-body :rear-thread)
-    (:top-wing :upper-body :wing-attachment)
-    (:bolt-2 :top-wing :middle-thread)
-    (:window :top-wing :window-attachment)
-    (:bolt-3 :window :window-thread)
-    (:top-wing :holder-plane-vertical :vertical-attachment)
-    (:propeller :motor-grill :propeller-attachment)
-    (:bolt-4 :propeller :propeller-thread)))
-
-(defun prepare (phase &key (only nil) (reset nil))
-  (declare (type number phase))
+(defun prepare (&key (all-preceeding t) (reset nil))
   (when reset (reset))
-  
-  )
+  (format t "Preparing all-preceeding phases: ~a~%" all-preceeding)
+  (format t "Resetting before execution: ~a~%" reset)
+  (format t "Choose phase to prepare:~%")
+  (loop for phase in *assembly-steps*
+        for id to (1- (length *assembly-steps*))
+        do (case (car phase)
+             (go-connect (format t "~a) ~a on ~a~%" id (second phase) (car (last phase))))
+             (btr:attach-object (format t "~a) ~a~%" id phase))
+             (:otherwise (format t "not-preparable - ~a~%" phase))))
+  (flet ((attach (name other-name attachment)
+           (btr:detach-all-objects (btr:object btr:*current-bullet-world* name))
+           (cram-occasions-events:on-event
+            (make-instance 'cpoe:object-attached-object
+                           :object-name name :other-object-name other-name
+                           :attachment-type attachment))
+           (visualize-part name)
+           (update-collision-scene name)))
+    (let ((phase-id (read)))
+      (if (<= 0 phase-id (1- (length *assembly-steps*)))
+          (if all-preceeding
+              (loop for pred-id to (1- phase-id)
+                    for pred = (nth pred-id *assembly-steps*)
+                    do (case (car pred)
+                         (go-connect (apply #'attach (mapcar (alexandria:rcurry #'nth pred) '(1 3 5))))
+                         (btr:attach-object (apply #'btr:attach-object (cdr pred)))))
+              (let ((phase (nth phase-id *assembly-steps*)))
+                (case (car phase)
+                  (go-connect (apply #'attach (mapcar (alexandria:rcurry #'nth phase) '(1 3 5))))
+                  (btr:attach-object (apply #'btr:attach-object (cdr phase)))
+                  (:otherwise (roslisp:ros-warn (assembly phase-mock) "Can't prepare ~a." phase-id)))))
+          (roslisp:ros-warn (assembly phase-mock) "Phase ~a out of range." phase-id)))))
 
+(defun mock-in-hand (?object-type)
+  (with-giskard-controlled-robot
+        (let* ((?object-designator 
+                 (exe:perform
+                  (desig:a motion
+                           (type world-state-detecting)
+                           (object (desig:an object (type ?object-type))))))
+               (manipulation-data
+                 (cut:lazy-car
+                  (prolog:prolog
+                  `(and 
+                    (desig:current-designator ,?object-designator ?current-object-desig)
+                    ;; (spec:property ?current-object-desig (:type ?object-type))
+                    (spec:property ?current-object-desig (:name ?object-name))
+                    (lisp-fun man-int:get-object-transform ?current-object-desig ?object-transform)
+                    (and (lisp-fun man-int:get-action-grasps ,?object-type :left ?object-transform ?grasps)
+                         (member ?grasp ?grasps))))))
+               (?name (cut:var-value '?object-name manipulation-data))
+               (grasp (cut:var-value '?grasp manipulation-data))
+               (pose-in-map
+                 (cram-tf:multiply-transform-stampeds
+                  "map" (roslisp-utilities:rosify-underscores-lisp-name ?name) 
+                  (cl-tf:lookup-transform cram-tf:*transformer* "map" "left_gripper_tool_frame")
+                  (cram-tf:transform-stamped-inv
+                   (man-int:get-object-type-to-gripper-transform ?object-type ?name :left grasp))
+                  :result-as-pose-or-transform :pose)))
+            (when (btr:attached-objects (btr:get-robot-object))
+              (btr:detach-all-objects (btr:get-robot-object)))
+            (btr:detach-all-objects (btr:object btr:*current-bullet-world* ?name))
+            (setf (btr:pose (btr:object btr:*current-bullet-world* ?name))
+                  pose-in-map)
+            (exe:perform
+             (desig:an action
+                       (type detecting)
+                       (object (desig:an object (name ?name)))))
+            (sleep 1)
+            (cram-occasions-events:on-event
+             (make-instance 'cpoe:object-attached-robot
+                            :grasp grasp
+                            :arm :left
+                            :object-name ?name)))))
+
+#+deprecated
+(
 (defun end-phase (phase &key (only nil) (reset nil))
   (declare (type number phase))
   (when reset (reset))
@@ -70,12 +127,7 @@
        (make-instance 'cpoe:object-attached-robot
                       :grasp :top
                       :arm :left
-                      :object-name :chassis))
-      ;; (exe:perform
-      ;;  (desig:an action
-      ;;           (type detecting)
-      ;;           (object (desig:an object (name :chassis)))))
-      )))
+                      :object-name :chassis)))))
 
 (defun bw-in-hand ()
   (let* ((pose-in-map
@@ -204,3 +256,4 @@
                       :grasp :top
                       :arm :left
                       :object-name :bolt-1)))))
+)
